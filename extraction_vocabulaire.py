39a2c25ff13c7a6df078662edd780ed3f885b58e
quentinverlhac <quentin.verlhac@gmail.com>
2020-04-02 19:10:59 +0200

    remove stop words

--- extraction_vocabulaire.py
+++ extraction_vocabulaire.py
@@ -1,5 +1,6 @@
 import collections
-from load_documents import load_data
+import configparser
+from load_documents import load_data, load_stop_words
 
 
 def article_tokenize_simple(text):
@@ -15,24 +16,32 @@ def article_tokenize_simple(text):
         return tokens
 
 
-def article_remove_stop_words(text_tokens,stop_word_file):
+def article_remove_stop_words(text_tokens, stop_words):
     """
-    removes words in stop_word_file from text_tokens
+    removes words in stop_words from text_tokens
     :param text_tokens: (list of string)
-    :param stop_word_file: (list of string)
+    :param stop_words: (list of string)
     :return: tokens (list of string)
     """
-    for word in stop_word_file:
+    for word in stop_words:
         text_tokens = list(filter(lambda a: a != word, text_tokens))
 
     return(text_tokens)
 
 
 def main():
-    corpus = load_data()
+    config = configparser.ConfigParser()
+    config.read_file(open(r'data_location.config'))
+    data_path = config.get('data_path', 'data_path')
+    stop_words_path = config.get('stop_words_path', 'stop_words_path')
+
+    corpus = load_data(data_path)
+    stop_words = load_stop_words(stop_words_path)
     for key in corpus:
         raw_words = article_tokenize_simple(corpus[key])
         print(collections.Counter(raw_words))
+        filtered_words = article_remove_stop_words(raw_words, stop_words)
+        print(len(raw_words) - len(filtered_words), "stop word removed")
         break
 
 
