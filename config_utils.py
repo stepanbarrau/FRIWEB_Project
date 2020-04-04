1accdd470dc8634ba53c7c16f974d1f271bfd502
quentinverlhac <quentin.verlhac@gmail.com>
2020-04-04 21:32:13 +0200

    fix config_utils

--- config_utils.py
+++ config_utils.py
@@ -3,10 +3,7 @@ import configparser
 
 
 def load_config():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    config_dir, _ = os.path.split(dir_path)
     config_file = r'data_location.config'
-    config_path = os.path.join(config_dir, config_file)
     config = configparser.ConfigParser()
-    config.read_file(open(config_path))
+    config.read_file(open(config_file))
     return config
