49a838675e3216099c13b17a3410ddd98fcf3cb6
Barrau Stepan <stepan.barrau@student.ecp.fr>
2020-04-04 19:45:13 +0200

    optimizations suggested by quentin

--- inverted_index.py
+++ inverted_index.py
@@ -1,13 +1,20 @@
 import collections
-import os
-from extraction_vocabulaire import article_tokenize_simple
-from load_documents import load_data
+from enum import Enum
+import pickle
+from extraction_vocabulaire import get_collection_from_corpus
+from load_documents import load_data, load_stop_words
+import configparser
 
 
-def build_index(corpus, type):
+class IndexType(Enum):
+    SIMPLE = 0
+    FREQUENCY = 1
+
+
+def build_index(collection, type):
     """
-    builds inverted index from corpus (dict:name->text) of texts
-    :param corpus: dict string -> string
+    builds inverted index from a collection of stemmed words (dict:name->text) of texts
+    :param collection: dict string -> string
     :param type: type of the index : type = 0 -> document index (simple)
                                      type  = 1 -> frequency index
                                      type = 3 -> position index (not yet implemented)
@@ -15,138 +22,78 @@ def build_index(corpus, type):
              df (dict term(string) -> frequency (int)
     """
 
-    if type not in [0, 1]:
-        raise Exception("type of inverted index not implemented")
-
     index = {}
     df = {}
-    for text_name in corpus:
-        print(index)
+    for text_name in collection:
+        term_counter = collections.Counter(collection[text_name])
 
-        raw_words = article_tokenize_simple(corpus[text_name])
-        term_counter = collections.Counter(raw_words)
-
-        if type == 0:
+        if type == IndexType.SIMPLE:
             for term in term_counter:
-                if term in index:
-                    index[term].append(text_name)
-                    df[term] += 1
-                else:
-                    index[term] = [text_name]
-                    df[term] = 1
+                index[term] = index.setdefault(term, [])
+                df[term] = df.setdefault(term, 0)
 
-        if type == 1:
+                index[term].append(text_name)
+                df[term] += 1
+
+        if type == IndexType.FREQUENCY:
             for term in term_counter:
-                if term in index:
-                    index[term].append((text_name, term_counter[term]))
-                    df[term] += 1
-                else:
-                    index[term] = [(text_name, term_counter[term])]
-                    df[term] = 1
+                index[term] = index.setdefault(term, [])
+                df[term] = df.setdefault(term, 0)
+
+                index[term].append((text_name, term_counter[term]))
+                df[term] += 1
 
     return index, df
 
 
-def save_index_to_file(index, df, filename, type):
-    """
-    WARNING: terms, or document names must NOT contain linebreaks, spaces, or the cheracters | and §
-    saves index to a text file, under the format:
-    Term1 df_1|doc_1 doc_4                              if it is a type 1
-    or
-    Term1 df_1|doc_1 tf_doc1_Term1§doc_4 tf_doc_4_Term1 if it is a type 2
-    :param index:
-    :param filename:
-    :return:
-    """
-    if type not in [0, 1]:
-        raise Exception("type of inverted index not implemented")
-
-    with open(filename, "w+") as f:
-
-        if type == 0:
-            for term in index:
-                line = ""
-                line += f"{term} {df[term]}|"
-                for doc_name in index[term]:
-                    line += f"{doc_name} "
-                line += "\n"
-                f.write(line)
-
-        if type == 1:
-            for term in index:
-                f.write(f"{term} {df[term]}|")
-                for doc in index[term]:
-                    doc_name, doc_tf = doc
-                    f.write(f"{doc_name} {doc_tf}§")
-                f.write(os.linesep)
-
+def save_index_to_file(inverted_index, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(inverted_index, f)
         f.close()
 
 
-def load_index_from_file(filename, type):
-    """
-    WARNING: terms, or document names must NOT contain linebreaks, spaces, or the cheracters | and §
-    loads index and df from file, under the above format
-    :param filename: string
-    :param type: int
-    :return: index, df
-    """
-    if type not in [0, 1]:
-        raise Exception("type of inverted index not implemented")
+def load_index_from_file(filename):
+    with open(filename, 'rb') as f:
+        index = pickle.load(f)
+        return index
 
-    index = {}
-    df = {}
 
-    if type == 0:
-        with open(filename, "r") as f:
-            for line in f:
-                tokens = line.split("|")
-                print(tokens)
-                header = tokens[0]
-                body = tokens[1]
-                header_tokens = header.split(" ")
-                body_tokens = body.split(" ")
-                term = header_tokens[0]
-                df_value = header_tokens[1]
-                df[term] = df_value
-                index[term] = []
-                for doc_name in body_tokens:
-                    index[term].append(doc_name)
+def save_df_to_file(df, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(df, f)
+        f.close()
 
-    if type == 1:
-        with open(filename, "r") as f:
-            for line in f:
-                tokens = line.split("|")
-                header = tokens[0]
-                body = tokens[1]
-                header_tokens = header.split(" ")
-                body_tokens = body.split("§")
-                term = header_tokens[0]
-                df_value = header_tokens[1]
-                df[term] = df_value
-                index[term] = []
-                for doc in body_tokens:
-                    doc_tokens = doc.split(" ")
-                    doc_name = doc_tokens[0]
-                    doc_tf = doc_tokens[1]
-                    index[term].append((doc_name, doc_tf))
 
-    return index, df
+def load_df_from_file(filename):
+    with open(filename, 'rb') as f:
+        df = pickle.load(f)
+        return df
 
 
 def test():
-    corpus = load_data()
-    index, df = build_index(corpus, 0)
-    save_index_to_file(index, df, "test_index_file3.txt", 0)
-    index_loaded, df_loaded = load_index_from_file("test_index_file3.txt", 0)
-    print(index_loaded)
-    print(df_loaded)
+    config = configparser.ConfigParser()
+    config.read_file(open(r'data_location.config'))
+    data_path = config.get('data_path', 'data_path')
+    stop_words_path = config.get('stop_words_path', 'stop_words_path')
+
+    corpus = load_data(data_path)
+    stop_words = load_stop_words(stop_words_path)
+
+    collection = get_collection_from_corpus(corpus, stop_words)
+    index, df = build_index(collection, IndexType.SIMPLE)
+
+    save_index_to_file(index, "test_index_file4.txt")
+    index_loaded = load_index_from_file("test_index_file4.txt")
+
+    save_df_to_file(df, "test_df_file4.txt")
+    df_loaded = load_df_from_file("test_df_file4.txt")
+
     no_error = True
     for term in index:
         if df[term] != df_loaded[term]:
             no_error = False
         for doc_name in index[term]:
-            if not doc_name in index_loaded[term]:
+            if doc_name not in index_loaded[term]:
                 no_error = False
 
     print(f"no_error : {no_error}")
