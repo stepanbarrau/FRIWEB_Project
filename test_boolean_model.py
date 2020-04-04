5069905b953d562c00129b1c8926b72e309da507
quentinverlhac <quentin.verlhac@gmail.com>
2020-04-04 22:19:33 +0200

    test boolean model

---
+++ test_boolean_model.py
@@ -0,0 +1,19 @@
+from config_utils import load_config
+from data_processing import load_stop_words
+from inverted_index import load_index_from_file
+from boolean_model import process_query_boolean
+
+if __name__ == "__main__":
+    config = load_config()
+    stop_words = load_stop_words(config.get(
+        'stop_words_path', 'stop_words_path'))
+    inv_index = load_index_from_file("test_index_file4.txt")
+    query1 = "stanford"
+    query2 = "stanford class avm"
+    query3 = "class"
+    result1 = process_query_boolean(query1, inv_index, stop_words)
+    result2 = process_query_boolean(query2, inv_index, stop_words)
+    result3 = process_query_boolean(query3, inv_index, stop_words)
+    print(result1)
+    print(result2)
+    print(result3)
