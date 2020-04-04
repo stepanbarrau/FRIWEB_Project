ec96571d6172c578c99c3de5595b4ac47bdba199
quentinverlhac <quentin.verlhac@gmail.com>
2020-04-04 18:12:10 +0200

    add general query process function

---
+++ models/query.py
@@ -0,0 +1,9 @@
+from extraction_vocabulaire import tokenize_simple, remove_stop_words, lemmatize, stemming
+
+
+def process_query(query, stop_words):
+    raw_words = tokenize_simple(query)
+    filtered_words = remove_stop_words(raw_words, stop_words)
+    lemmatized_words = lemmatize(filtered_words)
+    stemmed_words = stemming(lemmatized_words)
+    return stemmed_words
