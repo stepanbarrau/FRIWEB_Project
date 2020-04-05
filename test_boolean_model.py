1e0cf6bb370545f36779ae419555630e6a76f529
quentinverlhac <quentin.verlhac@gmail.com>
2020-04-05 14:42:05 +0200

    evaluate boolean results

--- test_boolean_model.py
+++ test_boolean_model.py
@@ -1,5 +1,5 @@
 from config_utils import load_config
-from data_processing import load_stop_words, pickle_load_from_file, save_results
+from data_processing import load_stop_words, pickle_load_from_file, save_results, load_results
 from boolean_model import process_query_boolean
 
 if __name__ == "__main__":
@@ -15,7 +15,27 @@ if __name__ == "__main__":
     result3 = process_query_boolean(query3, inv_index, stop_words)
     result4 = process_query_boolean(query4, inv_index, stop_words)
     result8 = process_query_boolean(query8, inv_index, stop_words)
-    save_results(result2, "data/boolean_results/result2")
-    save_results(result3, "data/boolean_results/result3")
-    save_results(result4, "data/boolean_results/result4")
-    save_results(result8, "data/boolean_results/result8")
+    save_results(result2, "data/boolean_results/result2.txt")
+    save_results(result3, "data/boolean_results/result3.txt")
+    save_results(result4, "data/boolean_results/result4.txt")
+    save_results(result8, "data/boolean_results/result8.txt")
+    load_result2 = load_results("data/queries/dev_output/2.out")
+    load_result3 = load_results("data/queries/dev_output/3.out")
+    load_result4 = load_results("data/queries/dev_output/4.out")
+    load_result8 = load_results("data/queries/dev_output/8.out")
+    result2_difference = len(list(
+        set(load_result2).symmetric_difference(set(result2))))
+    result3_difference = len(list(
+        set(load_result3).symmetric_difference(set(result3))))
+    result4_difference = len(list(
+        set(load_result4).symmetric_difference(set(result4))))
+    result8_difference = len(list(
+        set(load_result8).symmetric_difference(set(result8))))
+    print(
+        f"results 2: expected {len(load_result2)} elements - got {len(result2)} - {result2_difference} elements different")
+    print(
+        f"results 3: expected {len(load_result3)} elements - got {len(result3)} - {result3_difference} elements different")
+    print(
+        f"results 4: expected {len(load_result4)} elements - got {len(result4)} - {result4_difference} elements different")
+    print(
+        f"results 8: expected {len(load_result8)} elements - got {len(result8)} - {result8_difference} elements different")
