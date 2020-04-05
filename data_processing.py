22d3233516bec56b009ab7ea05e669a4340fb0bf
quentinverlhac <quentin.verlhac@gmail.com>
2020-04-05 15:20:07 +0200

    implement load_queries_and_output

--- data_processing.py
+++ data_processing.py
@@ -15,6 +15,23 @@ def load_data(data_path):
     return corpus
 
 
+def load_queries_and_output(query_path, query_output_path):
+    """
+    load all queries and their output in a dictionnary. Keys are queries and values are outputs.
+    :return: a dictionary with keys as queries (string) and content as output (list of string)
+    """
+    queries_and_outputs = {}
+    path = Path(query_path)
+    for p in path.glob('*'):
+        _, query_file_name = os.path.split(p)
+        query_number = query_file_name.split('.')[-1]
+        query = load_results(p)[0]
+        query_output = load_results(os.path.join(
+            query_output_path, f'{query_number}.out'))
+        queries_and_outputs[query] = query_output
+    return queries_and_outputs
+
+
 def load_stop_words(filename):
     with open(filename, 'r') as f:
         maj_stop_words = filter(lambda s: s != "", f.read().split("\n\n"))
