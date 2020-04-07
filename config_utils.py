7055070d83216e2211d333226f26c84fff617703
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-04-07 11:04:27 +0200

    Udpate config to seperate inverted invex and frequency index

--- config_utils.py
+++ config_utils.py
@@ -3,7 +3,7 @@ import configparser
 
 
 def load_config():
-    config_file = r'data_location.config'
+    config_file = r'data_location.template.config'
     config = configparser.ConfigParser()
     config.read_file(open(config_file))
     return config
