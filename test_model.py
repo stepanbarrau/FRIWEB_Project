cde0ead9b824722591c096ad35172302dff41a86
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-04-07 10:54:14 +0200

    Fix inverted index script for frequency index

--- test_model.py
+++ test_model.py
@@ -1,8 +1,14 @@
+from argparse import ArgumentParser
+
 from config_utils import load_config
 from data_processing import load_stop_words, pickle_load_from_file, load_queries_and_output
 from boolean_model import process_query_boolean
 
 if __name__ == "__main__":
+
+
+
+
     config = load_config()
     stop_words = load_stop_words(config.get(
         'stop_words_path', 'stop_words_path'))
