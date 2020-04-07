5a92d7d2ef80068435b452ee0ed39903d41e4d59
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-04-07 11:06:24 +0200

    Fix vectorial model and update test script

--- test_model.py
+++ test_model.py
@@ -1,25 +1,60 @@
 from argparse import ArgumentParser
 
 from config_utils import load_config
-from data_processing import load_stop_words, pickle_load_from_file, load_queries_and_output
+from data_processing import load_stop_words, pickle_load_from_file, pickle_save_data_to_file, load_queries_and_output
 from boolean_model import process_query_boolean
+from vectorial_model import process_vectorial_query, get_stats_collection
+
+
+def get_boolean_results(queries, inv_index, stop_words):
+    results = []
+    for query in queries:
+        results.append(process_query_boolean(query, inv_index, stop_words))
+    return results
+
+
+def get_vectorial_results(queries, inv_index, stop_words, stats_collection, args):
+    results = []
+    for query in queries:
+        results.append(process_vectorial_query(query, inv_index, stop_words, stats_collection,
+                                               args.weight_document, args.weight_query))
+    return results
+
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    subparser = parser.add_subparsers(dest="model")
+    boolean = subparser.add_parser("boolean")
+    vectorial = subparser.add_parser("vectorial")
 
-
-
-
+    vectorial.add_argument("--weight-query", type=str, choices=("boolean", "frequency"))
+    vectorial.add_argument("--weight-document", type=str,
+                           choices=("boolean", "frequency", "tf_idf_normalize", "tf_idf_logarithmic",
+                                    "tf_idf_logarithmic_normalize"))
+    args = parser.parse_args()
     config = load_config()
-    stop_words = load_stop_words(config.get(
-        'stop_words_path', 'stop_words_path'))
-    inv_index = pickle_load_from_file(config.get(
-        'simple_index_path', 'simple_index_path'))
+    stop_words = load_stop_words(config.get('stop_words_path', 'stop_words_path'))
+
     queries_and_output = load_queries_and_output(config.get('queries_path', 'queries_path'), config.get(
         'queries_output_path', 'queries_output_path'))
-    for query in queries_and_output:
-        prediction = process_query_boolean(query, inv_index, stop_words)
-        output = queries_and_output[query]
-        difference = list(
-            set(prediction).symmetric_difference(set(output)))
-        print(
-            f"query '{query}': expected {len(output)} elements - got {len(prediction)} - {len(difference)} elements different")
+
+    if args.model == "boolean":
+        inv_index = pickle_load_from_file(config.get('simple_index_path', 'simple_index_path'))
+        for query in queries_and_output:
+            prediction = process_query_boolean(query, inv_index, stop_words)
+            output = queries_and_output[query]
+            difference = list(
+                set(prediction).symmetric_difference(set(output)))
+            print(f"""query '{query}': expected {len(output)} elements - got {len(prediction)} - {len(difference)} 
+            elements different""")
+    elif args.model == "vectorial":
+        collection = pickle_load_from_file(config.get('collection_path', 'collection_path'))
+        frequency_index = pickle_load_from_file(config.get("frequency_index_path", "frequency_index_path"))
+        stats_collection = get_stats_collection(collection)
+        for query in queries_and_output:
+            prediction = get_vectorial_results(queries, frequency_index, stop_words, stats_collection, args)
+            output = queries_and_output[query]
+            difference = list(
+                set(prediction).symmetric_difference(set(output)))
+            print(f"""query '{query}': expected {len(output)} elements - got {len(prediction)} - {len(difference)} 
+            elements different""")
--- vectorial_model.py
+++ vectorial_model.py
@@ -2,84 +2,117 @@ import math
 from collections import Counter
 from collections import OrderedDict
 
-from collections_processing import article_tokenize_other, collection_lemmatize, remove_stop_words
-from data_processing import load_stop_words
+from collection_processing import article_tokenize_other, lemmatize, remove_stop_words
 
 
 def remove_non_index_term(query, inverted_index):
-    query_filt = []
+    """"Remove tokens that are not in index from query."""
+    query_filtered = []
     for token in query:
         if token in inverted_index:
-            query_filt.append(token)
-    return query_filt
+            query_filtered.append(token)
+    return query_filtered
 
 
-def pre_processed_query(query, inverted_index):
+def pre_processed_query(query, inverted_index, stop_words):
+    """
+    Process raw query for vectorial vectorial query.
+
+    Tokenize, filter stop words and lemmatize query.
+    :param query: (string) query
+    :param inverted_index:
+    :return:
+    """
     tokenized_query = article_tokenize_other(query)
-    filt_query = remove_non_index_term(tokenized_query, inverted_index)
-    filtered_query = remove_stop_words({"query": filt_query}, load_stop_words("./Data/Time/TIME.STP"))
-    normalized_query = collection_lemmatize(filtered_query)
-    return normalized_query["query"]
+    filtered_query = remove_non_index_term(tokenized_query, inverted_index)
+    filtered_query = remove_stop_words(filtered_query, stop_words)
+    normalized_query = lemmatize(filtered_query)
+    return normalized_query
 
 
-def get_tf(term, doc_ID, index_frequence):
-    return index_frequence[term][doc_ID]
+def get_term_frequency(term, doc_id, index_frequency):
+    """Get the frequency of a term in a document of the collection."""
+    return index_frequency[term][doc_id]
 
 
-def get_tf_logarithmique(term, doc_ID, index_frequence):
-    tf = get_tf(term, doc_ID, index_frequence)
+def get_logarithmic_term_frequency(term, doc_id, index_frequency):
+    """
+    Get logarithmic frequency of term in a document of the collection.
+
+    Equal to 0 if the term is not in the document, otherwise 1 + log(term_frequency).
+    """
+    tf = get_term_frequency(term, doc_id, index_frequency)
     if tf > 0:
         return 1 + math.log(tf)
     else:
         return 0
 
 
+def get_normalized_term_frequency(term, doc_id, index_frequency, stats_collection):
+    """Get normalized term frequency"""
+    term_frequency = get_term_frequency(term, doc_id, index_frequency)
+    normalized_term_frequency = 0.5 + 0.5 * (term_frequency / stats_collection[doc_id]["max_frequency"])
+    return normalized_term_frequency
+
+
+def get_normalized_logarithmic_term_frequency(term, doc_id, index_frequency, stats_collection):
+    """Get normalized logarithmic term frequency"""
+    term_frequency = get_term_frequency(term, doc_id, index_frequency)
+    normalized_logarithmic_term_frequency = (1 + math.log(term_frequency)) / (1 + math.log(
+        stats_collection[doc_id]["average_frequency"]))
+    return normalized_logarithmic_term_frequency
+
+
+def get_idf(term, index_frequency, nb_doc):
+    """Gets idf of term in collection"""
+    return math.log(nb_doc / len(index_frequency[term].keys()))
+
+
 def get_stats_document(document):
+    """
+    Get statistics on the document.
+
+    Calculates the frequency of most common term, the number of unique terms, and the average frequency of terms.
+    :param document: Document to get statistics on
+    :return: (dict) stats
+    """
     counter = Counter()
     for term in document:
         counter.update([term])
-    stats = {}
-    stats["freq_max"] = counter.most_common(1)[0][1]
-    stats["unique_terms"] = len(counter.items())
+    stats = dict()
+    stats["max_frequency"] = counter.most_common(1)[0][1]
+    stats["number_unique_terms"] = len(counter.items())
     tf_moy = sum(counter.values())
-    stats["freq_moy"] = tf_moy / len(counter.items())
+    stats["average_frequency"] = tf_moy / len(counter.items())
     return stats
 
 
 def get_stats_collection(collection):
-    stats = {}
+    """Get statistics on all the documents in the collection """
+    stats = dict()
     stats["nb_docs"] = len(collection.keys())
     for doc in collection:
         stats[doc] = get_stats_document(collection[doc])
     return stats
 
 
-def get_tf_normalise(term, doc_ID, index_frequence, stats_collection):
-    tf = get_tf(term, doc_ID, index_frequence)
-    tf_normalise = 0.5 + 0.5 * (tf / stats_collection[doc_ID]["freq_max"])
-    return tf_normalise
+def process_vectorial_query(query, frequency_index, stop_words, stats_collection, weighting_scheme_document,
+                            weighting_scheme_query):
+    # Set weighting schemes if not specified
+    if weighting_scheme_document is None:
+        weighting_scheme_document = "tf_idf_normalize"
+    if weighting_scheme_query is None:
+        weighting_scheme_query = "frequency"
 
-
-def get_tf_logarithme_normalise(term, doc_ID, index_frequence, stats_collection):
-    tf = get_tf(term, doc_ID, index_frequence)
-    tf_logarithme_normalise = (1 + math.log(tf)) / (1 + math.log(stats_collection[doc_ID]["freq_moy"]))
-    return tf_logarithme_normalise
-
-
-def get_idf(term, index_frequence, nb_doc):
-    return math.log(nb_doc / len(index_frequence[term].keys()))
-
-
-def processing_vectorial_query(query, inverted_index, stats_collection, weighting_scheme_document,
-                               weighting_scheme_query):
     relevant_docs = {}
     counter_query = Counter()
-    query_pre_processed = pre_processed_query(query, inverted_index)
-    print(query)
+    # Preprocess query
+    query_pre_processed = pre_processed_query(query, frequency_index, stop_words)
+
     nb_doc = stats_collection["nb_docs"]
     norm_query = 0.
     for term in query_pre_processed:
-        if term in inverted_index:
+        if term in frequency_index:
             w_term_query = 0.
             counter_query.update([term])
             if weighting_scheme_query == "binary":
@@ -87,26 +120,22 @@ def processing_vectorial_query(query, inverted_index, stats_collection, weightin
             if weighting_scheme_query == "frequency":
                 w_term_query = counter_query[term]
             norm_query = norm_query + w_term_query * w_term_query
-            for doc in inverted_index[term]:
+            for doc in frequency_index[term]:
                 w_term_doc = 0.
                 relevant_docs[doc] = 0.
                 if weighting_scheme_document == "binary":
                     w_term_doc = 1
                 if weighting_scheme_document == "frequency":
-                    w_term_doc = get_tf(term, doc, inverted_index)
+                    w_term_doc = get_term_frequency(term, doc, frequency_index)
                 if weighting_scheme_document == "tf_idf_normalize":
-                    w_term_doc = get_tf_normalise(term, doc, inverted_index, stats_collection) * get_idf(term,
-                                                                                                         inverted_index,
-                                                                                                         nb_doc)
+                    w_term_doc = get_normalized_term_frequency(term, doc, frequency_index, stats_collection
+                                                               ) * get_idf(term, frequency_index, nb_doc)
                 if weighting_scheme_document == "tf_idf_logarithmic":
-                    w_term_doc = get_tf_logarithmique(term, doc, inverted_index) * get_idf(term, inverted_index, nb_doc)
+                    w_term_doc = get_logarithmic_term_frequency(term, doc, frequency_index
+                                                                ) * get_idf(term, frequency_index, nb_doc)
                 if weighting_scheme_document == "tf_idf_logarithmic_normalize":
-                    w_term_doc = get_tf_logarithme_normalise(term, doc, inverted_index, stats_collection) * get_idf(
-                        term, inverted_index, nb_doc)
+                    w_term_doc = get_normalized_logarithmic_term_frequency(term, doc, frequency_index, stats_collection
+                                                                           ) * get_idf(term, frequency_index, nb_doc)
                 relevant_docs[doc] = relevant_docs[doc] + w_term_doc * w_term_query
     ordered_relevant_docs = OrderedDict(sorted(relevant_docs.items(), key=lambda t: t[1], reverse=True))
     return ordered_relevant_docs
-
-
-if __name__ == '__main__':
-
